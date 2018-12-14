e49322cec486787cabaef4c4133e94c4fd6e3a04
harddog <867129306@qq.com>
2018-12-14 19:32:51 +0800

    添加wkcustomprotocol hook

---
+++ Tools/MobileMiniBrowser/MobileMiniBrowserFramework/URLProtocol/FishMyHook.h
@@ -0,0 +1,18 @@
+//
+//  *@项目名称:  xywallet
+//  *@文件名称:  FishMyHook.h
+//  *@Date 2018/12/13
+//  *@Author lzh 
+//  *@Copyright © :  2014-2018 X-Financial Inc.   All rights reserved.
+//  *注意：本内容仅限于小赢科技有限责任公内部传阅，禁止外泄以及用于其他的商业目的。
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FishMyHook : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tools/MobileMiniBrowser/MobileMiniBrowserFramework/URLProtocol/FishMyHook.m
@@ -0,0 +1,50 @@
+//
+//  *@项目名称:  xywallet
+//  *@文件名称:  FishMyHook.m
+//  *@Date 2018/12/13
+//  *@Author lzh 
+//  *@Copyright © :  2014-2018 X-Financial Inc.   All rights reserved.
+//  *注意：本内容仅限于小赢科技有限责任公内部传阅，禁止外泄以及用于其他的商业目的。
+//
+
+#import "FishMyHook.h"
+#import <WebKit/WebKit.h>
+#import <objc/runtime.h>
+
+@implementation FishMyHook
+
++ (void)load {
+    if (([[[UIDevice currentDevice] systemVersion] doubleValue] >= 8.0)) {
+        const char *className = @"WKCustomProtocol".UTF8String;
+        Class WKCustomProtocolClass = objc_getClass(className);
+        SEL af_startLoading = NSSelectorFromString(@"fix_startLoading");
+
+        SEL startLoading = NSSelectorFromString(@"startLoading");
+
+        BOOL add1 = class_addMethod(WKCustomProtocolClass,
+                                    startLoading,
+                                    class_getMethodImplementation(WKCustomProtocolClass, startLoading),
+                                    "v@:");
+
+        BOOL add2 = class_addMethod(WKCustomProtocolClass,
+                                    af_startLoading,
+                                    (IMP)first_startLoading,
+                                    "v@:");
+
+        NSLog(@"%d,%d", add1, add2);
+
+        Method orgi = class_getInstanceMethod(WKCustomProtocolClass, startLoading);
+        Method after = class_getInstanceMethod(WKCustomProtocolClass, af_startLoading);
+        method_exchangeImplementations(orgi, after);
+    }
+}
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+void first_startLoading (id sender, SEL cmd) {
+    SEL startLoading = NSSelectorFromString(@"fix_startLoading");
+    [sender performSelector:startLoading];
+}
+#pragma clang diagnostic pop
+
+@end
